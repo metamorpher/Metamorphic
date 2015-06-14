cebd8ae14ff4bf38072cf40eb895318902d140a1
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-06-14 17:05:25 +1200

    Adding the unit tests for the core assembly

---
+++ src/Test.Unit.Core/Signals/SignalTest.cs
@@ -0,0 +1,131 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="Metamorphic">
+//     Copyright 2013 Metamorphic. Licensed under the Apache License, Version 2.0.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+
+namespace Metamorphic.Core.Signals
+{
+    [TestFixture]
+    [SuppressMessage("Microsoft.StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented",
+        Justification = "Unit tests do not need documentation.")]
+    public class SignalTest
+    {
+        [Test]
+        public void Construct()
+        {
+            var type = "a";
+            var signal = new Signal(type, new Dictionary<string, string>());
+
+            Assert.AreSame(type, signal.SignalType);
+        }
+
+        [Test]
+        public void ContainsParameterWithNullString()
+        {
+            var type = "a";
+            var parameters = new Dictionary<string, string>
+                {
+                    { "a", "b" }
+                };
+            var signal = new Signal(type, parameters);
+
+            Assert.IsFalse(signal.ContainsParameter(null));
+        }
+
+        [Test]
+        public void ContainsParameterWithEmptyString()
+        {
+            var type = "a";
+            var parameters = new Dictionary<string, string>
+                {
+                    { "a", "b" }
+                };
+            var signal = new Signal(type, parameters);
+
+            Assert.IsFalse(signal.ContainsParameter(string.Empty));
+        }
+
+        [Test]
+        public void ContainsParameterWithNonExistingParameter()
+        {
+            var type = "a";
+            var parameters = new Dictionary<string, string>
+                {
+                    { "a", "b" }
+                };
+            var signal = new Signal(type, parameters);
+
+            Assert.IsFalse(signal.ContainsParameter("c"));
+        }
+
+        [Test]
+        public void ContainsParameterWithExistingParameter()
+        {
+            var type = "a";
+            var parameters = new Dictionary<string, string>
+                {
+                    { "a", "b" }
+                };
+            var signal = new Signal(type, parameters);
+
+            Assert.IsTrue(signal.ContainsParameter("a"));
+        }
+
+        [Test]
+        public void ParameterValueWithNullString()
+        {
+            var type = "a";
+            var parameters = new Dictionary<string, string>
+                {
+                    { "a", "b" }
+                };
+            var signal = new Signal(type, parameters);
+
+            Assert.Throws<ParameterNotFoundException>(() => signal.ParameterValue(null));
+        }
+
+        [Test]
+        public void ParameterValueWithEmptyString()
+        {
+            var type = "a";
+            var parameters = new Dictionary<string, string>
+                {
+                    { "a", "b" }
+                };
+            var signal = new Signal(type, parameters);
+
+            Assert.Throws<ParameterNotFoundException>(() => signal.ParameterValue(string.Empty));
+        }
+
+        [Test]
+        public void ParameterValueWithNonExistingParameter()
+        {
+            var type = "a";
+            var parameters = new Dictionary<string, string>
+                {
+                    { "a", "b" }
+                };
+            var signal = new Signal(type, parameters);
+
+            Assert.Throws<ParameterNotFoundException>(() => signal.ParameterValue("c"));
+        }
+
+        [Test]
+        public void ParameterValueWithExistingParameter()
+        {
+            var type = "a";
+            var parameters = new Dictionary<string, string>
+                {
+                    { "a", "b" }
+                };
+            var signal = new Signal(type, parameters);
+
+            Assert.AreEqual("b", signal.ParameterValue("a"));
+        }
+    }
+}
