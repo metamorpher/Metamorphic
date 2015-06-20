d9b03c5f61a84dccb548e976e6323dc76866c51c
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-06-20 15:28:10 +1200

    Unit test project for the server assembly.

---
+++ src/Test.Unit.Server/SignalProcessorTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Unit.Server
+{
+    [TestClass]
+    public class SignalProcessorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
