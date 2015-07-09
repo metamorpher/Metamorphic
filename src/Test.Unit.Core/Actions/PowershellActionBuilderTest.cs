a76af99918fee7e7ff4ede57ea8e13aceb1aa159
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-07-09 22:38:57 +1200

    Writing unit tests for the actions

---
+++ src/Test.Unit.Core/Actions/PowershellActionBuilderTest.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="Metamorphic">
+//     Copyright 2013 Metamorphic. Licensed under the Apache License, Version 2.0.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using Nuclei.Diagnostics;
+using Nuclei.Diagnostics.Logging;
+using NUnit.Framework;
+
+namespace Metamorphic.Core.Actions
+{
+    [TestFixture]
+    public sealed class PowershellActionBuilderTest
+    {
+        [Test]
+        public void ToDefinition()
+        {
+            var output = string.Empty;
+            Action<LevelToLog, string> logger = (l, m) => 
+                {
+                    output = m;
+                };
+            var diagnostics = new SystemDiagnostics(logger, null);
+            var builder = new PowershellActionBuilder(diagnostics);
+
+            var definition = builder.ToDefinition();
+            Assert.AreEqual(new ActionId("powershell"), definition.Id);
+        }
+    }
+}
---
+++ src/Test.Unit.Server/Actions/ActionStorageTest.cs
@@ -0,0 +1,76 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="Metamorphic">
+//     Copyright 2013 Metamorphic. Licensed under the Apache License, Version 2.0.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using Metamorphic.Core.Actions;
+using NUnit.Framework;
+
+namespace Metamorphic.Server.Actions
+{
+    [TestFixture]
+    public sealed class ActionStorageTest
+    {
+        [Test]
+        public void Add()
+        {
+            var storage = new ActionStorage();
+
+            Action action = () => { };
+            var definition = new ActionDefinition(
+                new ActionId("a"),
+                new ActionParameterDefinition[0],
+                action);
+            storage.Add(definition);
+
+            Assert.IsTrue(storage.HasActionFor(definition.Id));
+            Assert.AreSame(definition, storage.Action(definition.Id));
+        }
+
+        [Test]
+        public void AddWithDuplicateDefinition()
+        {
+            var storage = new ActionStorage();
+
+            var id = new ActionId("a");
+            Action action = () => { };
+            var definition = new ActionDefinition(
+                id,
+                new ActionParameterDefinition[0],
+                action);
+            storage.Add(definition);
+
+            Assert.IsTrue(storage.HasActionFor(definition.Id));
+            Assert.AreSame(definition, storage.Action(definition.Id));
+
+            var otherDefinition = new ActionDefinition(
+                id,
+                new ActionParameterDefinition[0],
+                action);
+            Assert.Throws<DuplicateActionDefinitionException>(() => storage.Add(definition));
+        }
+
+        [Test]
+        public void AddWithNullDefinition()
+        {
+            var storage = new ActionStorage();
+            Assert.Throws<ArgumentNullException>(() => storage.Add(null));
+        }
+
+        [Test]
+        public void HasActionForWithNullId()
+        {
+            var storage = new ActionStorage();
+            Assert.IsFalse(storage.HasActionFor(null));
+        }
+
+        [Test]
+        public void HasActionForWithUnknownId()
+        {
+            var storage = new ActionStorage();
+            Assert.IsFalse(storage.HasActionFor(new ActionId("a")));
+        }
+    }
+}
