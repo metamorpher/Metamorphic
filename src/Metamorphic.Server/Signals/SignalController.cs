5cc888edd43a6cd7630f6dc912836648ebec31ff
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-07-16 12:00:36 +1200

    Fixing the web api methods to actually work

--- src/Metamorphic.Server/Signals/SignalController.cs
+++ src/Metamorphic.Server/Signals/SignalController.cs
@@ -11,11 +11,18 @@ using Metamorphic.Core.Signals;
 
 namespace Metamorphic.Server.Signals
 {
-    internal sealed class SignalController : ApiController
+    /// <summary>
+    /// The controller that handles signals.
+    /// </summary>
+    public sealed class SignalController : ApiController
     {
         private readonly IQueueSignals m_Queue;
 
-        public SignalController(IQueueSignals signalQueue)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SignalController"/> class.
+        /// </summary>
+        /// <param name="signalQueue">The queue that holds all the signals.</param>
+        internal SignalController(IQueueSignals signalQueue)
         {
             {
                 Lokad.Enforce.Argument(() => signalQueue);
@@ -24,6 +31,25 @@ namespace Metamorphic.Server.Signals
             m_Queue = signalQueue;
         }
 
+        /// <summary>
+        /// Pings the controller to see if it is alive.
+        /// </summary>
+        /// <returns>A http response message indicating whether the call was successful.</returns>
+        [HttpGet]
+        public HttpResponseMessage Ping()
+        {
+            return new HttpResponseMessage
+            {
+                StatusCode = System.Net.HttpStatusCode.OK,
+            };
+        }
+
+        /// <summary>
+        /// Triggers a new signal.
+        /// </summary>
+        /// <param name="signalType">The id of the signal.</param>
+        /// <param name="arguments">The arguments for the signal.</param>
+        /// <returns>A http response message indicating whether the call was successful.</returns>
         [HttpPost]
         public HttpResponseMessage Trigger(string signalType, string arguments)
         {
