57a9461db5b33e5548afc5efaddc0f1aac99380e
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-07-22 17:39:58 +1200

    adding the script that will trigger a build

---
+++ src/Metamorphic.Server/Scripts/start-build.ps1
@@ -0,0 +1,21 @@
+﻿[CmdletBinding()]
+param(
+    [ValidateNotNullOrEmpty()]
+    [string] $id,
+
+    [ValidateNotNullOrEmpty()]
+    [string] $user,
+
+    [ValidateNotNullOrEmpty()]
+    [string] $comment
+)
+
+# Expecting something like: Development complete. Test builds: [Cinema, FilmProgramming, HeadOffice] on branches [Main, branches/4.5.2]
+
+# parse the comment string
+
+
+$builds = @("a", "b")
+$branches = @("c", "d" )
+
+# Start bulids
