37c1f5dfe8dc791d24dfc7dcb3bf257843880619
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-06-15 11:29:12 +1200

    Adding unit tests for the SignalProcessor
    references #3

--- src/Test.Unit.Server/SignalProcessorTest.cs
+++ src/Test.Unit.Server/SignalProcessorTest.cs
@@ -5,9 +5,20 @@
 //-----------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
+using Metamorphic.Core.Actions;
+using Metamorphic.Core.Jobs;
+using Metamorphic.Core.Queueing;
+using Metamorphic.Core.Queueing.Signals;
+using Metamorphic.Core.Rules;
+using Metamorphic.Core.Signals;
+using Metamorphic.Server.Jobs;
+using Metamorphic.Server.Rules;
+using Moq;
+using Nuclei.Diagnostics;
 using NUnit.Framework;
 
-namespace Test.Unit.Server
+namespace Metamorphic.Server
 {
     [TestFixture]
     public class SignalProcessorTest
@@ -15,55 +26,172 @@ namespace Test.Unit.Server
         [Test]
         public void CreateWithNullJobQueue()
         {
-            throw new NotImplementedException();
+            var rules = new Mock<IStoreRules>();
+            var signals = new Mock<IDispenseSignals>();
+            var diag = new SystemDiagnostics((l, m) => { }, null);
+
+            Assert.Throws<ArgumentNullException>(() => new SignalProcessor(null, rules.Object, signals.Object, diag));
         }
 
         [Test]
         public void CreateWithNullRuleCollection()
         {
-            throw new NotImplementedException();
+            var jobs = new Mock<IQueueJobs>();
+            var signals = new Mock<IDispenseSignals>();
+            var diag = new SystemDiagnostics((l, m) => { }, null);
+
+            Assert.Throws<ArgumentNullException>(() => new SignalProcessor(jobs.Object, null, signals.Object, diag));
         }
 
         [Test]
         public void CreateWithNullSignalDispenser()
         {
-            throw new NotImplementedException();
+            var jobs = new Mock<IQueueJobs>();
+            var rules = new Mock<IStoreRules>();
+            var diag = new SystemDiagnostics((l, m) => { }, null);
+
+            Assert.Throws<ArgumentNullException>(() => new SignalProcessor(jobs.Object, rules.Object, null, diag));
         }
 
         [Test]
         public void CreateWithNullDiagnostics()
         {
-            throw new NotImplementedException();
+            var jobs = new Mock<IQueueJobs>();
+            var rules = new Mock<IStoreRules>();
+            var signals = new Mock<IDispenseSignals>();
+
+            Assert.Throws<ArgumentNullException>(() => new SignalProcessor(jobs.Object, rules.Object, signals.Object, null));
         }
 
         [Test]
         public void DispenseWithNullSignal()
         {
-            throw new NotImplementedException();
+            var jobs = new Mock<IQueueJobs>();
+            {
+                jobs.Setup(j => j.Enqueue(It.IsAny<Job>()))
+                    .Verifiable();
+            }
+
+            var rules = new Mock<IStoreRules>();
+            {
+                rules.Setup(r => r.RulesForSignal(It.IsAny<SignalTypeId>()))
+                    .Verifiable();
+            }
+
+            var signals = new Mock<IDispenseSignals>();
+            var diag = new SystemDiagnostics((l, m) => { }, null);
+
+            var processor = new SignalProcessor(jobs.Object, rules.Object, signals.Object, diag);
+
+            signals.Raise(s => s.OnItemAvailable += null, new ItemEventArgs<Signal>(null));
+            jobs.Verify(j => j.Enqueue(It.IsAny<Job>()), Times.Never());
+            rules.Verify(r => r.RulesForSignal(It.IsAny<SignalTypeId>()), Times.Never());
         }
 
         [Test]
         public void DispenseWithSignalWithNoMatchingRule()
         {
-            throw new NotImplementedException();
+            var jobs = new Mock<IQueueJobs>();
+            {
+                jobs.Setup(j => j.Enqueue(It.IsAny<Job>()))
+                    .Verifiable();
+            }
+
+            var rules = new Mock<IStoreRules>();
+            {
+                rules.Setup(r => r.RulesForSignal(It.IsAny<SignalTypeId>()))
+                    .Returns(new Rule[0])
+                    .Verifiable();
+            }
+
+            var signals = new Mock<IDispenseSignals>();
+            var diag = new SystemDiagnostics((l, m) => { }, null);
+
+            var processor = new SignalProcessor(jobs.Object, rules.Object, signals.Object, diag);
+
+            var type = new SignalTypeId("a");
+            var parameters = new Dictionary<string, object>
+                {
+                    { "a", "b" }
+                };
+            var signal = new Signal(type, parameters);
+
+            signals.Raise(s => s.OnItemAvailable += null, new ItemEventArgs<Signal>(signal));
+            jobs.Verify(j => j.Enqueue(It.IsAny<Job>()), Times.Never());
+            rules.Verify(r => r.RulesForSignal(It.IsAny<SignalTypeId>()), Times.Once());
         }
 
         [Test]
-        public void DispenseWithSignalWithoutParameters()
+        public void DispenseWithSignalWithSingleMatchingRule()
         {
-            throw new NotImplementedException();
-        }
+            var type = new SignalTypeId("a");
+            var parameters = new Dictionary<string, object>();
+            var signal = new Signal(type, parameters);
 
-        [Test]
-        public void DispenseWithSignalWithParameters()
-        {
-            throw new NotImplementedException();
+            var jobs = new Mock<IQueueJobs>();
+            {
+                jobs.Setup(j => j.Enqueue(It.IsAny<Job>()))
+                    .Verifiable();
+            }
+
+            var rules = new Mock<IStoreRules>();
+            {
+                var conditions = new Dictionary<string, Predicate<object>>();
+                var ruleParameters = new Dictionary<string, ActionParameterValue>();
+                rules.Setup(r => r.RulesForSignal(It.IsAny<SignalTypeId>()))
+                    .Returns(
+                        new Rule[]
+                        {
+                            new Rule("b", "c", type, new ActionId("d"), conditions, ruleParameters),
+                        })
+                    .Verifiable();
+            }
+
+            var signals = new Mock<IDispenseSignals>();
+            var diag = new SystemDiagnostics((l, m) => { }, null);
+
+            var processor = new SignalProcessor(jobs.Object, rules.Object, signals.Object, diag);
+
+            signals.Raise(s => s.OnItemAvailable += null, new ItemEventArgs<Signal>(signal));
+            jobs.Verify(j => j.Enqueue(It.IsAny<Job>()), Times.Exactly(1));
+            rules.Verify(r => r.RulesForSignal(It.IsAny<SignalTypeId>()), Times.Once());
         }
 
         [Test]
         public void DispenseWithSignalWithMultipleMatchingRules()
         {
-            throw new NotImplementedException();
+            var type = new SignalTypeId("a");
+            var parameters = new Dictionary<string, object>();
+            var signal = new Signal(type, parameters);
+
+            var jobs = new Mock<IQueueJobs>();
+            {
+                jobs.Setup(j => j.Enqueue(It.IsAny<Job>()))
+                    .Verifiable();
+            }
+
+            var rules = new Mock<IStoreRules>();
+            {
+                var conditions = new Dictionary<string, Predicate<object>>();
+                var ruleParameters = new Dictionary<string, ActionParameterValue>();
+                rules.Setup(r => r.RulesForSignal(It.IsAny<SignalTypeId>()))
+                    .Returns(
+                        new Rule[] 
+                        {
+                            new Rule("b", "c", type, new ActionId("d"), conditions, ruleParameters),
+                            new Rule("e", "f", type, new ActionId("g"), conditions, ruleParameters)
+                        })
+                    .Verifiable();
+            }
+
+            var signals = new Mock<IDispenseSignals>();
+            var diag = new SystemDiagnostics((l, m) => { }, null);
+
+            var processor = new SignalProcessor(jobs.Object, rules.Object, signals.Object, diag);
+
+            signals.Raise(s => s.OnItemAvailable += null, new ItemEventArgs<Signal>(signal));
+            jobs.Verify(j => j.Enqueue(It.IsAny<Job>()), Times.Exactly(2));
+            rules.Verify(r => r.RulesForSignal(It.IsAny<SignalTypeId>()), Times.Once());
         }
     }
 }
