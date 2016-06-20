ea579c17fc4d63db0edd6a535374e25332216ec8
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-06-20 23:51:15 +1200

    Adding the storage project
    references #22

---
+++ src/Metamorphic.Storage/DependencyInjection.cs
@@ -0,0 +1,137 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="Metamorphic">
+// Copyright (c) Metamorphic. All rights reserved.
+// Licensed under the Apache License, Version 2.0 license. See LICENCE.md file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Metamorphic.Core;
+using Nuclei.Configuration;
+using Nuclei.Diagnostics;
+using Nuclei.Diagnostics.Logging;
+using Nuclei.Diagnostics.Profiling;
+
+namespace Metamorphic.Storage
+{
+    /// <summary>
+    /// Creates the dependency injection container with all the required references.
+    /// </summary>
+    internal static class DependencyInjection
+    {
+        /// <summary>
+        /// The default name for the error log.
+        /// </summary>
+        private const string DefaultInfoFileName = "server.info.log";
+
+        /// <summary>
+        /// Creates the dependency injection container for the application.
+        /// </summary>
+        /// <returns>The container.</returns>
+        public static IContainer CreateContainer()
+        {
+            var builder = new ContainerBuilder();
+            {
+                builder.Register(c => new XmlConfiguration(
+                        StorageConfigurationKeys.ToCollection()
+                            .Append(CoreConfigurationKeys.ToCollection())
+                            .Append(DiagnosticsConfigurationKeys.ToCollection())
+                            .ToList(),
+                        StorageConstants.ConfigurationSectionApplicationSettings))
+                    .As<IConfiguration>()
+                    .SingleInstance();
+
+                builder.Register(c => new ApplicationConstants())
+                    .As<ApplicationConstants>();
+
+                builder.Register(c => new FileConstants(c.Resolve<ApplicationConstants>()))
+                    .As<FileConstants>();
+
+                RegisterDiagnostics(builder);
+                RegisterLoggers(builder);
+                RegisterRules(builder);
+            }
+
+            return builder.Build();
+        }
+
+        private static void RegisterDiagnostics(ContainerBuilder builder)
+        {
+            builder.Register(
+                c =>
+                {
+                    var loggers = c.Resolve<IEnumerable<ILogger>>();
+                    Action<LevelToLog, string> action = (p, s) =>
+                    {
+                        var msg = new LogMessage(p, s);
+                        foreach (var logger in loggers)
+                        {
+                            try
+                            {
+                                logger.Log(msg);
+                            }
+                            catch (NLog.NLogRuntimeException)
+                            {
+                                // Ignore it and move on to the next logger.
+                            }
+                        }
+                    };
+
+                    Profiler profiler = null;
+                    if (c.IsRegistered<Profiler>())
+                    {
+                        profiler = c.Resolve<Profiler>();
+                    }
+
+                    return new SystemDiagnostics(action, profiler);
+                })
+                .As<SystemDiagnostics>()
+                .SingleInstance();
+        }
+
+        private static void RegisterLoggers(ContainerBuilder builder)
+        {
+            var assemblyInfo = Assembly.GetExecutingAssembly().GetName();
+            builder.Register(c => LoggerBuilder.ForFile(
+                    Path.Combine(c.Resolve<FileConstants>().LogPath(), DefaultInfoFileName),
+                    new DebugLogTemplate(c.Resolve<IConfiguration>(), () => DateTimeOffset.Now),
+                    assemblyInfo.Name,
+                    assemblyInfo.Version))
+                .As<ILogger>()
+                .SingleInstance();
+        }
+
+        private static void RegisterRules(ContainerBuilder builder)
+        {
+            builder.Register(c => new RuleCollection())
+                .As<IStoreRules>()
+                .SingleInstance();
+
+            builder.Register(
+                    c =>
+                    {
+                        var ctx = c.Resolve<IComponentContext>();
+                        return new RuleLoader(
+                            (string id) => ctx.Resolve<IStoreActions>().HasActionFor(new ActionId(id)),
+                            c.Resolve<SystemDiagnostics>());
+                    })
+                .As<ILoadRules>()
+                .SingleInstance();
+
+            builder.Register(c => new RuleWatcher(
+                    c.Resolve<IConfiguration>(),
+                    c.Resolve<ILoadRules>(),
+                    c.Resolve<IStoreRules>(),
+                    c.Resolve<SystemDiagnostics>()))
+                .As<IWatchRules>()
+                .SingleInstance();
+        }
+    }
+}
